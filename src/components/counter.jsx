55f50001e4e5a329d170126dbd1b93aac5d9983d
Sean Leif Hellier <sean@newtropolis.com>
2020-10-14 10:57:33 -0400

    exercise 5 - imbedding expressions

---
+++ src/components/counter.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+	state = {
+		count: 0,
+		imageURL: "https://picsum.photos/200",
+	};
+
+	render() {
+		return (
+			<div>
+				<img src={this.state.imageURL} alt="image" />
+				<span>{this.formatCount()}</span>
+				<button>Increment</button>
+			</div>
+		);
+	}
+
+	formatCount() {
+		const { count } = this.state;
+		return count === 0 ? "Zero" : count;
+	}
+}
+
+export default Counter;
