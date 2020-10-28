87ecfe2ead348ae570541aaaa5172c2325265ce7
Sean Leif Hellier <sean@newtropolis.com>
2020-10-27 21:19:39 -0400

    extension test code

---
+++ chrome-extension-test/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import HomePage from "./HomePage";
+import "./App.css";
+import { KeywordStore } from "./store";
+import Navbar from "react-bootstrap/Navbar";
+const keywordStore = new KeywordStore();
+function App() {
+  return (
+    <div className="App">
+      <Navbar bg="primary" expand="lg" variant="dark">
+        <Navbar.Brand href="#home">Weather App</Navbar.Brand>
+      </Navbar>
+      <HomePage keywordStore={keywordStore} />
+    </div>
+  );
+}
+export default App;
